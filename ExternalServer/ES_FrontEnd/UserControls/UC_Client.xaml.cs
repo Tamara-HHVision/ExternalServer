c67efafdd233f5fe3daab6c6bf52757031262856
Tamara-HHVision <graovac@hh-vision.de>
2023-05-24 14:22:11 +0200

    Client Settings --DESIGN ONLY---
    - Client Settings Tab designed
    - Client List Items designed

---
+++ ExternalServer/ES_FrontEnd/UserControls/UC_Client.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ES_FrontEnd.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UC_Client.xaml
+    /// </summary>
+    public partial class UC_Client : UserControl
+    {
+        public UC_Client()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
