b140d856029cf423149f243b49e15ee68cef5e90
Tamara-HHVision <graovac@hh-vision.de>
2023-05-25 15:02:22 +0200

    Add new Client Window
    - Just the design
    - Combobox seems to be bugging around much

--- ExternalServer/ES_FrontEnd/UserControls/UC_Client.xaml.cs
+++ ExternalServer/ES_FrontEnd/UserControls/UC_Client.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ES_FrontEnd.UserControls
 {
@@ -25,7 +13,7 @@ namespace ES_FrontEnd.UserControls
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void OnBtn_AddClient_Click(object sender, RoutedEventArgs e)
         {
 
         }
---
+++ ExternalServer/ES_FrontEnd/Windows/W_AddNewClient.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ES_FrontEnd.Windows
+{
+    /// <summary>
+    /// Interaction logic for W_AddNewClient.xaml
+    /// </summary>
+    public partial class W_AddNewClient : Window
+    {
+        public W_AddNewClient()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
