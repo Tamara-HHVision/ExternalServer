c182b886ec4162b48663d45d15238ac37a8c5453
Tamara-HHVision <graovac@hh-vision.de>
2023-07-04 14:43:42 +0200

    What's on the menu? Tabs, tabs, tabs
    - Tabs have been modified to do a lot of cool and neat-looking shit*
    - Sadly hover effect only works before any of the buttons get clicked qwq
    - Error found: somehow the scenario buttons don't get displayed At least the lower two of them...
    - BUT their styles got also changed on hovering and selecting, etc....

--- ExternalServer/ES_FrontEnd/MainWindow.xaml.cs
+++ ExternalServer/ES_FrontEnd/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ES_FrontEnd
 {
@@ -8,6 +10,8 @@ namespace ES_FrontEnd
     public partial class MainWindow : Window
     {
         private Controller m_controller;
+
+        private Button m_lastPressedButton;
 
         public MainWindow()
         {
@@ -19,21 +23,83 @@ namespace ES_FrontEnd
         private void OnBtn_ScenarioControl_Click(object sender, RoutedEventArgs e)
         {
             m_controller.ShowScenarioControl();
+
+            // Revert the background color of the last pressed button, if available
+            if (m_lastPressedButton != null)
+            {
+                m_lastPressedButton.Background = Brushes.Transparent;
+                m_lastPressedButton.BorderBrush = Brushes.Transparent;
+            }
+
+            // Set the background color of the current button
+            Button button = (Button)sender;
+            button.Background = new SolidColorBrush(Color.FromRgb(0, 0, 0)); // Custom Color code: Black
+            button.BorderBrush = new SolidColorBrush(Color.FromRgb(250, 150, 0)); // custom Color code: Orange
+
+            // Update the last pressed button
+            m_lastPressedButton = button;
+
+            button.UpdateLayout();
         }
 
         private void OnBtn_ClientSettings_Click(object sender, RoutedEventArgs e)
         {
             m_controller.ShowClientSettings();
+
+            // Revert the background color of the last pressed button, if available
+            if (m_lastPressedButton != null)
+            {
+                m_lastPressedButton.Background = Brushes.Transparent;
+                m_lastPressedButton.BorderBrush = Brushes.Transparent;
+            }
+
+            // Set the background color of the current button
+            Button button = (Button)sender;
+            button.Background = new SolidColorBrush(Color.FromRgb(0, 0, 0)); // Custom Color code: Black
+            button.BorderBrush = new SolidColorBrush(Color.FromRgb(250, 150, 0)); // custom Color code: Orange
+
+            // Update the last pressed button
+            m_lastPressedButton = button;
         }
 
         private void OnBtn_WeatherSettings_Click(object sender, RoutedEventArgs e)
         {
             m_controller.ShowWeatherSettings();
+
+            // Revert the background color of the last pressed button, if available
+            if (m_lastPressedButton != null)
+            {
+                m_lastPressedButton.Background = Brushes.Transparent;
+                m_lastPressedButton.BorderBrush = Brushes.Transparent;
+            }
+
+            // Set the background color of the current button
+            Button button = (Button)sender;
+            button.Background = new SolidColorBrush(Color.FromRgb(0, 0, 0)); // Custom Color code: Black
+            button.BorderBrush = new SolidColorBrush(Color.FromRgb(250, 150, 0)); // custom Color code: Orange
+
+            // Update the last pressed button
+            m_lastPressedButton = button;
         }
 
         private void OnBtn_ShipSettings_Click(object sender, RoutedEventArgs e)
         {
             m_controller.ShowShipSettings();
+
+            // Revert the background color of the last pressed button, if available
+            if (m_lastPressedButton != null)
+            {
+                m_lastPressedButton.Background = Brushes.Transparent;
+                m_lastPressedButton.BorderBrush = Brushes.Transparent;
+            }
+
+            // Set the background color of the current button
+            Button button = (Button)sender;
+            button.Background = new SolidColorBrush(Color.FromRgb(0, 0, 0)); // Custom Color code: Black
+            button.BorderBrush = new SolidColorBrush(Color.FromRgb(250, 150, 0)); // custom Color code: Orange
+
+            // Update the last pressed button
+            m_lastPressedButton = button;
         }
     }
 }
