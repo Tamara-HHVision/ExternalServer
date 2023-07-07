c8d5d76e23fc14e01c8359f1fb2bee1193782e14
Tamara-HHVision <graovac@hh-vision.de>
2023-07-07 15:29:47 +0200

    Status Log added
    - use Status.Log("") to make a label pop up with a message inside
    - can be used from anywhere
    - currently set to disappear after 3 seconds again
    - check Status.cs for more information

---
+++ ExternalServer/ES_FrontEnd/Status.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace ES_FrontEnd
+{
+    static class Status
+    {
+        private static Label s_Label;
+
+        public static void Init(Label _label)
+        {
+            s_Label = _label;
+        }
+
+        public static void Log(string _message)
+        {
+            if (s_Label == null)
+            {
+                MessageBox.Show("Status Label or Popup was not assigned!", "Status", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            s_Label.Content = _message;
+            s_Label.Visibility = Visibility.Visible;
+
+            DispatcherTimer timer = new DispatcherTimer();
+
+            // Duration of timer
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += (sender, e) =>
+            {
+                s_Label.Visibility = Visibility.Hidden; s_Label.Content = _message;
+                timer.Stop();
+            };
+
+            timer.Start();
+        }
+    }
+}
